898fb76c3899fe6bbaec401cb1ca5095a5f63002
Le Nguyen Hai Dang <danglnh07.work@gmail.com>
2025-11-03 12:10:34 +0700

    Fix error in worker main test: add new dependencies

--- service/worker/main_test.go
+++ service/worker/main_test.go
@@ -2,9 +2,11 @@ package worker
 
 import (
 	"context"
+	"fmt"
 	"os"
 	"strings"
 	"tekticket/db"
+	"tekticket/service/bot"
 	"tekticket/service/notify"
 	"tekticket/util"
 	"testing"
@@ -36,10 +38,16 @@ func TestMain(m *testing.M) {
 	}
 
 	mailService := notify.NewEmailService(os.Getenv("EMAIL"), os.Getenv("APP_PASSWORD"))
+	ablyService, err := notify.NewAblyService(os.Getenv("ABLY_API_KEY"))
+	if err != nil {
+		util.LOGGER.Error("failed to create ably service for test", "error", err)
+		os.Exit(1)
+	}
+	bot, err := bot.NewChatbot(os.Getenv("TELEGRAM_BOT_TOKEN"), fmt.Sprintf("%s/api/webhook/telegram", os.Getenv("SERVER_DOMAIN")))
 
 	config := &util.Config{
 		ResetPasswordURL: "http://localhost:3000", // Just some dump value, we only care about the token in this test
 	}
-	processor = NewRedisTaskProcessor(asynq.RedisClientOpt{Addr: os.Getenv("REDIS_ADDR")}, queries, mailService, config)
+	processor = NewRedisTaskProcessor(asynq.RedisClientOpt{Addr: os.Getenv("REDIS_ADDR")}, queries, mailService, ablyService, bot, config)
 	os.Exit(m.Run())
 }
